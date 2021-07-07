aadfb5f15129a3053cb97ee6ad42dc67381fd0a5
sheilashix <70675711+sheilashix@users.noreply.github.com>
2021-07-07 16:38:43 +0300

    Add files via upload

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+// sequelize model, datatypes, and database connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// the Post model extends  sequelize  
+class Post extends Model {}
+// define the table columns and configuration
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                               len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+// Export the model
+module.exports = Post;
