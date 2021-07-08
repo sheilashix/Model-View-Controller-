6d5cb5242feb6b51af1cfcfeec67d44d9690e28c
sheilashix <70675711+sheilashix@users.noreply.github.com>
2021-07-08 10:41:34 +0300

    Update add-post.js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,13 +1,9 @@
 // New Post Form Handler
 async function newFormHandler(event) {
     event.preventDefault();
-
-    // Get the post title and post text from the form
     const title = document.querySelector('input[name="post-title"]').value;
     const post_text = document.querySelector('textarea[name="post-text"]').value;
-
-    // use the add a new post POST route to add the post 
-    // user id is added from the session information in the route
+// use the add a new post POST route to add the post 
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
@@ -27,6 +23,4 @@ async function newFormHandler(event) {
       alert(response.statusText);
     }
   }
-  
-  // Event Listener for the new post submit button
   document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
